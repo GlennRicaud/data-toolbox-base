96f63d71fc4ab904f9db14791b0688a90ba212ab
Glenn Ricaud <gri@enonic.com>
2021-05-09 15:25:58 +0200

    Navigability - Anchors

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-mvn clean install && cd ../data-toolbox-app && ./gradlew clean deploy && cd ../data-toolbox-base
+echo "Deploying Data Toolbox (incl. updated DT Base)..."
+export XP_HOME=$XP_HOME_DATATOOLBOX
+export PATH=/usr/local/bin:$PATH
+mvn clean install -q && cd ../data-toolbox-app && ./gradlew clean deploy -q
+echo "Data Toolbox (incl. updated DT Base) deployed"
