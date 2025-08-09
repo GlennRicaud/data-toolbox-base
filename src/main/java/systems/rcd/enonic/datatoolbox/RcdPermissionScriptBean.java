05083b38a94de44f32597d66294b187e3bc1d97a
GlennRicaud <glenn.ricaud@gmail.com>
2025-08-09 17:31:04 +0200

    XP 8 migration: applyPermissions

--- src/main/java/systems/rcd/enonic/datatoolbox/RcdPermissionScriptBean.java
+++ src/main/java/systems/rcd/enonic/datatoolbox/RcdPermissionScriptBean.java
@@ -1,13 +1,5 @@
 package systems.rcd.enonic.datatoolbox;
 
-import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import systems.rcd.fwk.core.format.json.RcdJsonService;
-import systems.rcd.fwk.core.format.json.data.RcdJsonObject;
-
 import com.enonic.xp.branch.Branch;
 import com.enonic.xp.node.ApplyNodePermissionsParams;
 import com.enonic.xp.node.ApplyNodePermissionsResult;
@@ -19,6 +11,13 @@ import com.enonic.xp.security.PrincipalKey;
 import com.enonic.xp.security.acl.AccessControlEntry;
 import com.enonic.xp.security.acl.AccessControlList;
 import com.enonic.xp.security.acl.Permission;
+import systems.rcd.fwk.core.format.json.RcdJsonService;
+import systems.rcd.fwk.core.format.json.data.RcdJsonObject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class RcdPermissionScriptBean
     extends RcdScriptBean
@@ -38,17 +37,14 @@ public class RcdPermissionScriptBean
 
             final ApplyNodePermissionsParams params = ApplyNodePermissionsParams.create().
                 nodeId( NodeId.from( nodeId ) ).
-                inheritPermissions( inheritPermissions ).
-                overwriteChildPermissions( overwriteChildPermissions ).
                 permissions( toAccessControlList( permissions ) ).
                 build();
-            final ApplyNodePermissionsResult applicationResult =
+            final ApplyNodePermissionsResult applyNodePermissionsResult =
                 createContext( RepositoryId.from( repositoryName ), Branch.from( branchName ) ).
                     callWith( () -> nodeServiceSupplier.get().applyPermissions( params ) );
 
             final RcdJsonObject result = RcdJsonService.createJsonObject().
-                put( "succeedNodes", applicationResult.getSucceedNodes().getSize() ).
-                put( "skippedNodes", applicationResult.getSkippedNodes().getSize() );
+                put( "modifiedNodes", applyNodePermissionsResult.getResults().size() );
 
             return createSuccessResult( result );
         }, "Error while applying permissions" );
