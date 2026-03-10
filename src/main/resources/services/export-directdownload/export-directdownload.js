dcfd6129c763e694808c77b7cc73e4794f3e8b1a
GlennRicaud <glenn.ricaud@gmail.com>
2026-03-10 21:54:12 +0100

    XP 8 migration: Export direct download

--- src/main/resources/services/export-directdownload/export-directdownload.js
+++ src/main/resources/services/export-directdownload/export-directdownload.js
@@ -1,11 +1,13 @@
+const portalLib = require('/lib/xp/portal');
+
 exports.post = function (req) {
-    const bean = __.newBean('systems.rcd.enonic.datatoolbox.RcdDumpScriptBean');
+    const bean = __.newBean('systems.rcd.enonic.datatoolbox.RcdExportScriptBean');
     const archiveName = req.params.archiveName;
     const fileName = req.params.fileName;
 
     return {
         contentType: 'application/octet-stream',
-        body: bean.download(archiveName),
+        body: bean.directDownload(archiveName),
         headers: {
             "Content-Disposition": "attachment; filename=" + fileName
         }
