443f0b3aa9258fd471fc35864b375f5609d7acfa
GlennRicaud <glenn.ricaud@gmail.com>
2026-03-19 17:57:38 +0100

    XP 8 migration: Iteration 2: Fix breaking change and display of dialogs

--- src/main/java/systems/rcd/enonic/datatoolbox/RcdDumpScriptBean.java
+++ src/main/java/systems/rcd/enonic/datatoolbox/RcdDumpScriptBean.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
@@ -198,7 +197,7 @@ public class RcdDumpScriptBean
                 maxVersions( maxVersions ).
                 listener( createSystemDumpListener() );
             if (repositoryId != null) {
-                params.repositories( RepositoryIds.from(Collections.singleton(repositoryId) ) );
+                params.repositories( RepositoryIds.from(repositoryId));
             }
 
             final SystemDumpResult systemDumpResult = dumpServiceSupplier.get().dump( params.build() );
